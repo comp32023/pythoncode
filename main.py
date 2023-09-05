056c089f93010e7ee8b0a5c273458c2c1aea20b6
comp32023 <comp32023@mail.ru>
2023-09-05 12:33:55 +0300

    pythoncode

---
+++ main.py
@@ -0,0 +1,20 @@
+
+a = int(input('Введите число: '));
+b = str(input('Введите строку: '));
+c = list(range(1,a))
+d = [];
+print('Hello world!', c);
+for xxx in c:
+    if xxx % 2 == 0:
+        d.append(xxx)
+    else:
+        d.append(b)
+print(d)
+
+#x1 = int(input('Введите первое число: '))
+#x2 = int(input('Введите второе число: '))
+##x3 = int(input('Введите третье число: '))
+#a*(x*x)+bx+c=0;
+#D=(b*b)-4*a*c;
+# x1=-(b-coren(D))/(2*a);
+# x2=-(b+coren(D))/(2*a);
